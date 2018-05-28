0e9b1d722bce6c5f7a5abf0682a83b2baa3d6d1d
davidrajdgl <daviddgl2013@gmail.com>
2018-05-28 16:36:27 +0530

    Completed the Peer Review assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## creates a special "matrix", which is really a list containing a function to
+
+# 1. set the value of the matrix
+# 2. get the value of the matrix
+# 3.set the value of the  inverse
+# 4.get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL 
+  set <- function(y){
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve){ m <<- solve}
+  getinv <- function() m 
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
 }
 
 
-## Write a short comment describing this function
+## The following function calculates the inverse of the special "matrix" created with the above function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  if(!is.null(m)){
+    message('getting the cached data')
+    return(m)
+  }
+  data <-x$get()
+  m = solve(data,...)
+  x$setinv(m)
+  m
 }
